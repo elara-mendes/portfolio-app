b5b0f4b8d89d4c8f0e4328ca4b1bd51330a56175
Elara <elaramendes@yahoo.com>
2025-02-24 22:30:02 -0300

    Created two more columns for the projects.
    - I did this before the teacher to learn more.

--- Home.py
+++ Home.py
@@ -1,4 +1,8 @@
+import os
+from itertools import islice
+
 import streamlit as st
+import csv
 
 st.set_page_config("wide")
 
@@ -15,3 +19,29 @@ with col2:
     I'm a cybersecurity student which is exploring the Python world. 💕
     """
     st.info(text)
+
+st.write("Below you can find the projects that I'm working on, feel free to contact me.")
+
+images_folder = "images"
+
+col3, col4 = st.columns(2)
+
+with col3:
+    with open("data.csv", "r") as file:
+        reader = list(csv.DictReader(file, delimiter=";"))
+        for row in reader[:10]:
+            st.subheader(row["title"])
+            st.write(row["description"])
+            folder_path = os.path.join(images_folder, row["image"])
+            st.image(folder_path)
+            st.markdown(f"[Source Code]({row['url']})")
+
+with col4:
+    with open("data.csv", "r") as file:
+        reader = list(csv.DictReader(file, delimiter=";"))
+        for row in reader[10:]:
+            st.subheader(row["title"])
+            st.write(row["description"])
+            folder_path = os.path.join(images_folder, row["image"])
+            st.image(folder_path)
+            st.markdown(f"[Source Code]({row['url']})")
